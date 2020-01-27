571af36382ae568e98d08c5c83d13c9723501cdf
Jeffrey Brown <brownjs03@gmail.com>
2020-01-27 02:10:56 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    Artist.find_or_create_by(name: "Drake")
-    self.artist = "Drake"
+    artist = Artist.find_or_create_by(name: "Drake")
+    self.artist = artist
     #self.artist = Artist.find_or_create_by(name: "Drake")
   end
 end
